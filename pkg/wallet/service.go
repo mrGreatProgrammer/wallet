af01811635ad248f3a3f9b3c3b8f8462a12134cf
Student <fhgf9048@gmail.com>
2021-11-23 20:39:59 +0500

    лекция 13 закончена

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,8 +1,98 @@
 package wallet
 
-import "github.com/mrGreatProgrammer/wallet/pkg/types"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/mrGreatProgrammer/wallet/pkg/types"
+	// "github.com/pkg/errors"
+)
+
+type Error string
+
+func (e Error) Error() string {
+	return string(e)
+}
+
+var ErrPhoneRegistered = errors.New("phone already registered")
+var ErrAmountMustBePositive = errors.New("amount must be greater than zero")
+var ErrAccountNotFound = errors.New("account not found")
+var ErrNotEnoughBalance = errors.New("not enough balance")
 
 type Service struct {
-	accounts []types.Account
-	payments types.Payment
+	nextAcccountID int64
+	accounts []*types.Account
+	payments []*types.Payment
+}
+
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+
+	s.nextAcccountID++
+	account := &types.Account{
+		ID: s.nextAcccountID,
+		Phone: phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+	return account, nil
+}
+
+func (s *Service) Deposit(accountID int64, amount types.Money) error {
+	if amount <= 0 {
+		return ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return ErrAccountNotFound
+	}
+
+	// зачисление средств пока не рассматриваем как платёж
+	account.Balance += amount
+	return nil
+}
+
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+	if account == nil {
+		return nil,  ErrAccountNotFound
+	}
+
+	if account.Balance < amount {
+		return nil, ErrNotEnoughBalance
+	}
+
+	account.Balance -= amount
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID: paymentID,
+		AccountID: accountID,
+		Amount: amount,
+		Category: category,
+		Status: types.PaymentStatusInProgress,
+	}
+	s.payments = append(s.payments, payment)
+	return payment, nil
 }
