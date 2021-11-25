721168f3e1f4839354e56a78339e7b9e834386ad
Student <fhgf9048@gmail.com>
2021-11-25 09:36:06 +0500

    добавленны функции Reject, FindPaymetnById и другие измениня

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -1,9 +1,11 @@
+// page 26 лк 14
 package wallet
 
 import (
 	"reflect"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/mrGreatProgrammer/wallet/pkg/types"
 )
 
@@ -38,3 +40,87 @@ func TestService_FindAccountByID_notFound(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 }
+
+func TestService_Reject_success(t *testing.T) {
+	// создаём сервис
+	s := newTestService()
+	_, payments, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// пробуем отменить платёж
+	payment := payments[0]
+	err = s.Reject(payment.ID)
+	if err != nil {
+		t.Errorf("Reject(): error = %v", err)
+		return
+	}
+
+	savedPayment, err := s.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("Reject(): cant find payment by id, error = %v", err)
+		return
+	}
+	if savedPayment.Status != types.PaymentStatusFail {
+		t.Errorf("Reject(): status didnt changed, payment = %v", savedPayment)
+		return
+	}
+	savedAccount, err := s.FindAccountByID(payment.AccountID)
+	if err != nil {
+		t.Errorf("Reject(): cant find account by id, error = %v", err)
+		return
+	}
+	if savedAccount.Balance != defaultTestAccount.balance {
+		t.Errorf("Reject(): balance didnt changed, account = %v", savedAccount)
+		return
+	}
+}
+
+func TestService_FindPaymentByID_seccess(t *testing.T) {
+	// создаем сервис
+	s := newTestService()
+	_, payments, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// пробуем найти платёж
+	payment := payments[0]
+	got, err := s.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Errorf("FindPaymentByID(): error = %v", err)
+		return
+	}
+
+	// сравниваем платежи
+	if !reflect.DeepEqual(payment, got) {
+		t.Errorf("FindPaymentByID(): wrong payment returned = %v", err)
+		return
+	}
+}
+
+func TestService_FindPaymentByID_fail(t *testing.T) {
+	// созаём сервис
+	s := newTestService()
+	_, _, err := s.addAccount(defaultTestAccount)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// пробуем найти несуществуюший платёж
+	_, err = s.FindPaymentByID(uuid.New().String())
+	if err == nil {
+		t.Error("FindPaymentByID(): must return error, returned nil")
+		return
+	}
+
+	if err != ErrPaymentNotFound {
+		t.Errorf("FindPaymentByID(): must return ErrPaymentNotFound, returned = %v", err)
+		return
+	}
+}
+
